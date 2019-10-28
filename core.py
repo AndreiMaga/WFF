585670cbc6c461a446fd799eae349360ee0eae3d
Andrei Maga <andreimariomaga@gmail.com>
2019-10-28 20:11:24 +0200

    Added evaluate_all_interpretations function to Core

--- core.py
+++ core.py
@@ -51,3 +51,26 @@ class Core():
         if(self.evaluator.was_set == False):
             self.evaluator.setValues(self.syntax.root)
         return self.evaluator.evaluate(self.syntax.root)
+
+    def evaluate_all_interpretations(self):
+        if(self.syntax.root == None):
+            self.parse()
+        p = 2 ** len(self.evaluator.eval_config.keys()) - 1 # number of bits we need
+        lbits = len(bin(p).split("b")[1])
+        rez = []
+        while p != -1:
+            bits = [int(c) for c in bin(p).split("b")[1]]
+            while( len(bits) < lbits):
+                bits.insert(0, 0)
+            i = 0
+            l = []
+            for k in self.evaluator.eval_config.keys():
+                self.evaluator.eval_config[k] = bool(bits[i])
+                l.append(bool(bits[i]))
+                i += 1
+            self.evaluator.was_set = False
+            rez.append((l,self.evaluate()))
+            p -= 1
+        return rez
+
+        
--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 from core import Core
 from node import Node
+from pprint import pprint
 
-
-input_phrase = "(((P&(!Q))@(Q@R))&(P&R))"
+input_phrase = "((P&R)|Q)"
 config = {
-    'P' : True,
-    'Q' : False,
-    'R' : False
+    'P' : False,
+    'R' : False,
+    'Q' : True
 }
 
 def __init__():
     c = Core(input_phrase)
     c.createEvaluator(config)
-    print(c.evaluate())
+    c.check_syntax()
+    #print(c.evaluate())
+    pprint(c.evaluate_all_interpretations())
 
 
 if __name__ == "__main__":
